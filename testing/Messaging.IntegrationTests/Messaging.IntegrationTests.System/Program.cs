b0bdeedc43f7081e27de3e778629e3572c168e4d
Tomek Masternak <tomasz.masternak@particular.net>
2021-02-10 11:44:36 +0100

    integration tests scafolding

---
+++ testing/Messaging.IntegrationTests/Messaging.IntegrationTests.System/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace Messaging.IntegrationTests.System
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var (endpoint, _) = await StartEndpoint();
+
+            await endpoint.SendLocal(new PlaceOrder());
+
+            Console.WriteLine("Press any <key> to exit.");
+            Console.ReadKey();
+        }
+
+        public static async Task<(IEndpointInstance, OrderStore)> StartEndpoint()
+        {
+            var endpointName = "IntegrationTests.Endpoint";
+            
+            var configuration = new EndpointConfiguration(endpointName);
+            
+            var transport = configuration.UseTransport<LearningTransport>();
+            transport.Routing().RouteToEndpoint(typeof(PlaceOrder), endpointName);
+
+            var orderStore = new OrderStore();
+            configuration.RegisterComponents(cc => cc.RegisterSingleton(orderStore));
+
+            var endpoint = await Endpoint.Start(configuration).ConfigureAwait(false);
+
+            return (endpoint, orderStore);
+        }
+    }
+
+    public class PlaceOrder : ICommand
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class FinalizeOrder : ICommand
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class OrderStore
+    {
+        public List<Guid> PlacedOrders = new List<Guid>();
+    }
+
+    public class PlaceOrderHandler : IHandleMessages<PlaceOrder>,
+        IHandleMessages<FinalizeOrder>
+    {
+        readonly OrderStore store;
+
+        public PlaceOrderHandler(OrderStore store)
+        {
+            this.store = store;
+        }
+
+        public async Task Handle(PlaceOrder message, IMessageHandlerContext context)
+        {
+            Console.WriteLine("Order received");
+
+            var options = new SendOptions();
+            options.DelayDeliveryWith(TimeSpan.FromSeconds(5));
+            options.RouteReplyToThisInstance();
+
+            await context.SendLocal(new FinalizeOrder{Id = message.Id});
+        }
+
+        public Task Handle(FinalizeOrder message, IMessageHandlerContext context)
+        {
+            Console.WriteLine("Finalizing order");
+
+            store.PlacedOrders.Add(message.Id);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ testing/Messaging.IntegrationTests/Messaging.IntegrationTests.Tests/IntegrationTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Messaging.IntegrationTests.System;
+using NServiceBus;
+using NUnit.Framework;
+
+namespace Messaging.IntegrationTests.Tests
+{
+    public class IntegrationTests
+    {
+        IEndpointInstance endpoint;
+        OrderStore store;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            (endpoint, store) = await Program.StartEndpoint();
+        }
+
+        [Test]
+        public async Task PlaceOrder()
+        {
+            var id = Guid.NewGuid();
+            await endpoint.Send(new PlaceOrder{Id = id});
+
+            Assert.Contains(id, store.PlacedOrders);
+        }
+    }
+}
