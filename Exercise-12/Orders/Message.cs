2cd7e983bfe7b739f478ef874469c4ec99b4eff2
Szymon Pobiega <szymon.pobiega@gmail.com>
2021-05-11 21:56:53 +0200

    Message and OutboxState

---
+++ Exercise-12/Orders/Message.cs
@@ -0,0 +1,15 @@
+﻿public class Message
+{
+    public string Id { get; set; }
+    public string Payload { get; set; }
+
+    public Message()
+    {
+    }
+
+    public Message(string id, string payload)
+    {
+        Id = id;
+        Payload = payload;
+    }
+}
