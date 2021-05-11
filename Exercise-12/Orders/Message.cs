cf33be350521e5eb56f62f4c8889ac0143906e71
Szymon Pobiega <szymon.pobiega@gmail.com>
2021-05-11 21:58:11 +0200

    Message

--- Exercise-12/Orders/Message.cs
+++ Exercise-12/Orders/Message.cs
@@ -1,13 +1,13 @@
 ﻿public class Message
 {
     public string Id { get; set; }
-    public string Payload { get; set; }
+    public object Payload { get; set; }
 
     public Message()
     {
     }
 
-    public Message(string id, string payload)
+    public Message(string id, object payload)
     {
         Id = id;
         Payload = payload;
