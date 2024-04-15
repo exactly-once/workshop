0103b8d01ccfb3c1672f89d3e924878736fcd70a
Tomek Masternak <tomasz.masternak@particular.net>
2024-04-15 17:47:03 +0200

    ex8

--- Exercise-8/Frontend/DuplicateMessagesBehavior.cs
+++ Exercise-8/Frontend/DuplicateMessagesBehavior.cs
@@ -8,14 +8,13 @@ class DuplicateMessagesBehavior : Behavior<IOutgoingLogicalMessageContext>
     public override async Task Invoke(IOutgoingLogicalMessageContext context, Func<Task> next)
     {
         await next();
-        if (context.Message.Instance is AddItem addMeat && addMeat.Filling == Filling.Meat)
+        if (context.Message.Instance is AddItem {Filling: Filling.Meat})
         {
             await next();
         }
-        else if (context.Message.Instance is AddItem addMushrooms 
-                 && addMushrooms.Filling == Filling.Mushrooms)
+        else if (context.Message.Instance is AddItem {Filling: Filling.Mushrooms})
         {
-            await Task.Delay(TimeSpan.FromSeconds(10));
+            await Task.Delay(TimeSpan.FromSeconds(10), context.CancellationToken);
             await next();
         }
         
