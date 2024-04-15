4f7a9df85d0f224c4c1fed14f1f3326ddb40191a
Tomek Masternak <tomasz.masternak@particular.net>
2024-04-15 18:52:42 +0200

    ex1

--- Exercise-1/Orders/ConsistentInMemoryStore.cs
+++ Exercise-1/Orders/ConsistentInMemoryStore.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
 
 public class ConsistentInMemoryStore<T>
     where T : class, IEntity
